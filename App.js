c81a18f06797c32af8bc0c92a2a805063192afd8
RedAl_123 <ahm3d_lulat@hotmail.com>
2022-02-02 09:50:04 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'; 
+import { Text, View } from 'react-native';
+
+
+class SayHello extends Component {
+  render() {
+
+    return (
+      <View>
+        <Text>Hello, {this.props.name} 3!</Text>
+      </View>
+    );
+
+  }
+}
+
+class HelloWorldApp extends Component {
+  render() {
+    
+  let name = "Ahmed";
+  
+  return (
+    <View>
+      <SayHello name="Ahmed"/>
+      <SayHello name="Mike"/>
+      <SayHello name="World"/>
+      <SayHello name="Ash"/>
+      <SayHello name="Bob"/>
+    </View> 
+  );
+}
+}
+
+export default HelloWorldApp;
